4315d7063f2839b860b7117296116e748138aff3
marsim0 <marb@mail.ru>
2015-05-29 17:45:23 +0300

    0

--- PodProject/ViewController.m
+++ PodProject/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking/AFNetworking.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self getGroupId];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) getGroupId {
+    NSURL * url = [NSURL URLWithString:@"https://api.vk.com/method/"];
+    AFHTTPRequestOperationManager * reqManager = [[AFHTTPRequestOperationManager alloc]initWithBaseURL:url];
+    NSDictionary * params = [[NSDictionary alloc]initWithObjectsAndKeys:
+                             @"-50537266_122",@"posts",
+                             @1,@"extended",
+                             nil];
+    
+    [reqManager GET:@"wall.getById" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if ([responseObject isKindOfClass:[NSDictionary class]]) {
+            NSLog(@"%@", [[[responseObject valueForKey:@"response"] valueForKey:@"groups"]valueForKey:@"gid"] );
+        } else {
+            NSLog(@"uknown class");
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error");
+    }];
+     
 }
 
 @end
